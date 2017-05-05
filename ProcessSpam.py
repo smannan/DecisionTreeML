8f5753ef3d43da61db618ed3e63af9b3cd54ba75
Clay Jacobs <clayjacobs245@gmail.com>
2017-05-04 20:26:32 -0700

    Should get meta entity posts.

--- ProcessSpam.py
+++ ProcessSpam.py
@@ -41,7 +41,7 @@ def getTopMetaEntities():
       entPostCounts[str(entity["id"])] = 0
       for entId in entity["ent_ids"]:
          idParts = entId.split('_')
-         for entId in entitiesData[idParts]["ips"]
+         for entIp in entitiesData[idParts]["ips"]:
             for potName in pots:
                uids =[]
                for user in usersData[potName]:
