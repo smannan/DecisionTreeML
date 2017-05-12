58091b8a2135619d2f92c0b47b5e4fc3b6c9a465
Clay Jacobs <clayjacobs245@gmail.com>
2017-05-11 21:45:51 -0700

    ProcessSpam.py updates added

--- ProcessSpamQuestions.py
+++ ProcessSpamQuestions.py
@@ -164,7 +164,7 @@ class ProcessSpam:
       if docType == "content":
          processedRecord["author_id"] = str(record["author_id"])
          for author in self.allUsers:
-            colName = "author_id_" + author
+            colName = "author_id_" + str(author)
             processedRecord[colName] = 1 if author == record["author_id"] else 0
          
          # titleWords = self.parseTextBlock(record["title"], self.topwords)
