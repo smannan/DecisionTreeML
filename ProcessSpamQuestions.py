16e02060781631509157eccb8b09d17cc42c494b
Clay Jacobs <clayjacobs245@gmail.com>
2017-05-11 23:00:01 -0700

    ProcessSpam.py updates added

--- ProcessSpamQuestions.py
+++ ProcessSpamQuestions.py
@@ -163,6 +163,7 @@ class ProcessSpam:
       
       if docType == "content":
          #processedRecord["author_id"] = str(record["author_id"])
+         """
          for author in self.allUsers:
             colName = "author_id_" + str(author)
             processedRecord[colName] = 1 if author == record["author_id"] else 0
@@ -174,6 +175,7 @@ class ProcessSpam:
          for word in sorted(self.titleVocab):
             colName = "title_word_" + word
             processedRecord[colName] = titleWords.count(word) if word in titleWords else 0
+         """
          
          # textWords = self.parseTextBlock(record["text"], self.topwords)
          textWords = re.sub(r'[\.;:,\-!\?]', r'', record["text"]). \
