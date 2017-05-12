09990ce5f96cd6145049c18029949b38c2808008
Clay Jacobs <clayjacobs245@gmail.com>
2017-05-12 16:09:28 -0700

    ProcessSpam.py updates added

--- ProcessSpamQuestions.py
+++ ProcessSpamQuestions.py
@@ -217,7 +217,7 @@ class ProcessSpam:
             else:
                textWordCounts[word] += val[1]["text_word_"+word]
       wordCounts = sorted([(v, k) for (k, v) in textWordCounts.items()], reverse=True)
-      print([v for (k, v) in wordCounts[:20]].join(", "))
+      print(", ".join([v for (k, v) in wordCounts[:20]]))
       #postCounts = sorted([(v, k) for (k, v) in textLenAndHits], reverse=True)
       #print(postCounts[:60])
 
